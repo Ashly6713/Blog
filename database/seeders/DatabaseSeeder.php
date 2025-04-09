7668774809094148c80b3eb74d2571ab8aac7f3f
Ashly6713 <moreli.macne@gmail.com>
2025-04-09 17:10:59 -0400

    5to avance: ceracion Post y datos aleatorios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,5 +23,6 @@ class DatabaseSeeder extends Seeder
             'password'=> bcrypt('12345678')
         ]);
         Category::factory(5)->create();
+        Post::factory(100)->create();
     }
 }
